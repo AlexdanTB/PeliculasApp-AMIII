c9b615997f480a06655d6b6916465f9302448343
AlexdanTB <alexdanieltb220@gmail.com>
2026-06-07 18:19:01 -0500

    feat: creación de screens catalogo y reproductor

---
+++ lib/screens/Catalogo.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CatalogoScreen extends StatelessWidget {
+  const CatalogoScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Text('Catálogo'));
+  }
+}
---
+++ lib/screens/Reproductor.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ReproductorScreen extends StatelessWidget {
+  const ReproductorScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Text('Reproductor de películas'));
+  }
+}
