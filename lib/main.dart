616d8bd52e4411ff8f1ceaf63ff202878b12aa42
AlexdanTB <alexdanieltb220@gmail.com>
2026-06-07 09:16:42 -0500

    feat: navegacion stack, se agrega a main

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:peliculas_app/navigations/StackNavigation.dart';
 
 void main() {
   runApp(PeliculasApp());
@@ -18,6 +19,6 @@ class Cuerpo extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(body: Stacknavigation());
   }
 }
---
+++ lib/navigations/StackNavigation.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:peliculas_app/screens/Login.dart';
+
+class Stacknavigation extends StatelessWidget {
+  const Stacknavigation({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: LoginScreen());
+  }
+}
