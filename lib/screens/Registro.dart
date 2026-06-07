2886ca70cc037f1e63bd25298feacf47f519b23e
AlexdanTB <alexdanieltb220@gmail.com>
2026-06-07 12:57:57 -0500

    feat: maquetacion incial screen Registro

--- lib/screens/Registro.dart
+++ lib/screens/Registro.dart
@@ -5,6 +5,50 @@ class RegistroScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Text('Registro'));
+    return Scaffold(
+      body: Column(
+        spacing: 10,
+        children: [
+          Text('Registrate', style: TextStyle(fontSize: 25)),
+          Text('Nombre de usuario'),
+          TextField(
+            decoration: InputDecoration(
+              label: Text('Escribe tu nick/usuario'),
+              border: OutlineInputBorder(),
+            ),
+          ),
+          Text('Correo Electrónico'),
+          TextField(
+            decoration: InputDecoration(
+              label: Text('Ingresa tu correo electrónico'),
+              border: OutlineInputBorder(),
+            ),
+          ),
+          Text('Fecha de nacimiento'),
+          Text('Contraseña'),
+          TextField(
+            decoration: InputDecoration(
+              label: Text('Ingresa tu contraseña'),
+              border: OutlineInputBorder(),
+            ),
+          ),
+          TextField(
+            decoration: InputDecoration(
+              label: Text('Confirma tu contraseña'),
+              border: OutlineInputBorder(),
+            ),
+          ),
+          FilledButton.icon(
+            onPressed: () => {},
+            label: Text('Registrar'),
+            icon: Icon(Icons.upgrade_sharp),
+          ),
+          TextButton(
+            onPressed: () => {},
+            child: Text('¿Ya tienes una cuenta? Inicia sesión ahora'),
+          ),
+        ],
+      ),
+    );
   }
 }
