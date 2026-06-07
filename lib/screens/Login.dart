8ae5b905dc42c967eb7e61014e7e034d6730f124
AlexdanTB <alexdanieltb220@gmail.com>
2026-06-07 12:44:47 -0500

    feat: maquetación inicial de screen Login

--- lib/screens/Login.dart
+++ lib/screens/Login.dart
@@ -5,6 +5,32 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Text('Login'));
+    return Scaffold(
+      body: Column(
+        spacing: 10,
+        children: [
+          Text('Iniciar Sesión', style: TextStyle(fontSize: 25)),
+          Text('Correo Electrónico'),
+          TextField(
+            decoration: InputDecoration(
+              label: Text('Ingresa tu correo electrónico'),
+              border: OutlineInputBorder(),
+            ),
+          ),
+          Text('Contraseña'),
+          TextField(
+            decoration: InputDecoration(
+              label: Text('Ingresa tu contraseña'),
+              border: OutlineInputBorder(),
+            ),
+          ),
+          FilledButton.icon(
+            onPressed: () => {},
+            label: Text('Inicia sesión'),
+            icon: Icon(Icons.login),
+          ),
+        ],
+      ),
+    );
   }
 }
