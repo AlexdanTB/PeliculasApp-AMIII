f098ad6733f3575d39dee5c0256ef2cb1ca88edf
AlexdanTB <alexdanieltb220@gmail.com>
2026-06-09 18:07:52 -0500

    feat: mostrar lista con futurebuilder y listview

--- lib/screens/catalogo_screen.dart
+++ lib/screens/catalogo_screen.dart
@@ -7,13 +7,44 @@ class CatalogoScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Text('Catálogo'));
+    return Scaffold(
+      body: Column(
+        children: [
+          Text('Catálogo de películas'),
+          Expanded(child: listaPeliculas(context)),
+        ],
+      ),
+    );
   }
 }
 
-Future<List> leerPeliculasLocal(context) async {
+Future<List> leerPeliculasLocal(BuildContext context) async {
   String jsonStr = await DefaultAssetBundle.of(
     context,
   ).loadString("assets/data/peliculas2.json");
-  return jsonDecode(jsonStr);
+  return jsonDecode(jsonStr)['peliculas'];
+}
+
+Widget listaPeliculas(BuildContext context) {
+  return FutureBuilder(
+    future: leerPeliculasLocal(context),
+    builder: (context, snapshot) {
+      if (snapshot.hasData) {
+        final data = snapshot.data!;
+        return ListView.builder(
+          itemCount: data.length,
+          itemBuilder: (context, index) {
+            final item = data[index];
+            return ListTile(
+              title: Text("${item['titulo']}"),
+              subtitle: Text("${item['director']}"),
+              leading: Image.network(item['imagen']),
+            );
+          },
+        );
+      } else {
+        return (CircularProgressIndicator());
+      }
+    },
+  );
 }
