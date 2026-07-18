969b6e672b13d00c242b4ac31e1682c7ac655f11
AlexdanTB <alexdanieltb220@gmail.com>
2026-07-18 09:17:56 -0500

    feat: leer datos usuario logeado

--- lib/navigations/BottomNav.dart
+++ lib/navigations/BottomNav.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:peliculas_app/screens/catalogo_screen.dart';
 import 'package:peliculas_app/screens/perfil_screen.dart';
-import 'package:peliculas_app/screens/reproductor_screen.dart';
 
 class Bottomnav extends StatefulWidget {
   const Bottomnav({super.key});
--- lib/screens/perfil_screen.dart
+++ lib/screens/perfil_screen.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 
 class PerfilScreen extends StatefulWidget {
@@ -9,17 +11,36 @@ class PerfilScreen extends StatefulWidget {
 
 class _PerfilScreenState extends State<PerfilScreen> {
   @override
+  void initState() {
+    leerDatosUser();
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         children: [
           CircleAvatar(
             radius: 50,
-            backgroundImage: AssetImage('assets/images/usuario.png'),
+            backgroundImage: AssetImage('assets/images/user.png'),
             backgroundColor: Color.fromRGBO(50, 50, 50, 1),
           ),
+          Text('data'),
         ],
       ),
     );
+  }
+}
+
+Future<void> leerDatosUser() async {
+  User? user = FirebaseAuth.instance.currentUser;
+  String userId = user!.uid;
+  final ref = FirebaseDatabase.instance.ref();
+  final snapshot = await ref.child('usuarios/$userId').get();
+  if (snapshot.exists) {
+    print(snapshot.value);
+  } else {
+    print('No data available. ${userId}');
   }
 }
