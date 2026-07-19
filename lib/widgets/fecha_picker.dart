2591577a5a040b3ff57236d3f775d3259e19f5a9
AlexdanTB <alexdanieltb220@gmail.com>
2026-07-18 22:35:31 -0500

    refact: Textfield independiente para fecha de nacimiento

---
+++ lib/widgets/fecha_picker.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class FechaPicker extends StatefulWidget {
+  final TextEditingController fechaController;
+  const FechaPicker(this.fechaController, {super.key});
+
+  @override
+  State<FechaPicker> createState() => _FechaPickerState();
+}
+
+class _FechaPickerState extends State<FechaPicker> {
+  DateTime? _fecha;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      readOnly: true,
+      controller: widget.fechaController,
+      decoration: InputDecoration(icon: Icon(Icons.date_range_rounded)),
+      keyboardType: TextInputType.datetime,
+      onTap: () => _fechanacimiento(context),
+    );
+  }
+
+  Future<void> _fechanacimiento(BuildContext context) async {
+    DateTime? fechapicked = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2000),
+      lastDate: DateTime.now(),
+    );
+
+    if (fechapicked != null && fechapicked != _fecha) {
+      setState(() {
+        _fecha = fechapicked;
+
+        String d = fechapicked.day.toString().padLeft(2, '0');
+        String m = fechapicked.month.toString().padLeft(2, '0');
+        String y = fechapicked.year.toString();
+
+        widget.fechaController.text = '$d/$m/$y';
+      });
+    }
+  }
+}
