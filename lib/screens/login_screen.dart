ff0925906ded7d7dbaefcb2717e5a28fd56f30d4
AlexdanTB <alexdanieltb220@gmail.com>
2026-07-19 19:43:50 -0500

    feat(login): mostrar y ocultar contraseña

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -2,8 +2,17 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:peliculas_app/screens/registro_sceen.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  bool mostrarPass = true;
+  TextEditingController correoC = TextEditingController();
+  TextEditingController constrasenaC = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -22,96 +31,101 @@ class LoginScreen extends StatelessWidget {
       ),
     );
   }
-}
 
-Widget formulario(context) {
-  TextEditingController correoC = TextEditingController();
-  TextEditingController constrasenaC = TextEditingController();
-  return Column(
-    spacing: 20,
-    children: [
-      Text(
-        'Iniciar Sesión',
-        style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
-      ),
-      TextField(
-        controller: correoC,
-        decoration: InputDecoration(
-          label: Text('Ingresa tu correo electrónico'),
-          enabledBorder: OutlineInputBorder(
-            borderSide: BorderSide(
-              color: Color.fromRGBO(75, 75, 75, 1),
-              width: 1.0,
-            ),
-          ),
-          focusedBorder: OutlineInputBorder(
-            borderSide: BorderSide(
-              color: Color.fromRGBO(163, 16, 75, 1),
-              width: 1.0,
-            ),
-          ),
-          prefixIcon: Icon(Icons.mail),
+  Widget formulario(context) {
+    return Column(
+      spacing: 20,
+      children: [
+        Text(
+          'Iniciar Sesión',
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
         ),
-      ),
-      TextField(
-        controller: constrasenaC,
-        decoration: InputDecoration(
-          label: Text('Ingresa tu contraseña'),
-          enabledBorder: OutlineInputBorder(
-            borderSide: BorderSide(
-              color: Color.fromRGBO(75, 75, 75, 1),
-              width: 1.0,
-            ),
-          ),
-          focusedBorder: OutlineInputBorder(
-            borderSide: BorderSide(
-              color: Color.fromRGBO(163, 16, 75, 1),
-              width: 1.0,
-            ),
-          ),
-          suffixIcon: IconButton(
-            onPressed: () => {},
-            icon: Icon(Icons.remove_red_eye),
-          ),
-        ),
-      ),
-      Container(
-        margin: EdgeInsets.all(5),
-        child: Column(
-          children: [
-            FilledButton.icon(
-              onPressed: () => login(context, correoC, constrasenaC),
-              label: Text('Inicia sesión'),
-              icon: Icon(Icons.login),
-            ),
-            TextButton(
-              onPressed: () => Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => RegistroScreen()),
+        TextField(
+          controller: correoC,
+          decoration: InputDecoration(
+            label: Text('Ingresa tu correo electrónico'),
+            enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(
+                color: Color.fromRGBO(75, 75, 75, 1),
+                width: 1.0,
               ),
-              child: Text('¿No tienes una cuenta? Registrate aquí'),
             ),
-          ],
+            focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(
+                color: Color.fromRGBO(163, 16, 75, 1),
+                width: 1.0,
+              ),
+            ),
+            prefixIcon: Icon(Icons.mail),
+          ),
         ),
-      ),
-    ],
-  );
-}
-
-Future<void> login(context, _correo, _contrasena) async {
-  String correo = _correo.text;
-  String contrasena = _contrasena.text;
-  try {
-    final credential = await FirebaseAuth.instance.signInWithEmailAndPassword(
-      email: correo,
-      password: contrasena,
+        TextField(
+          obscureText: mostrarPass,
+          controller: constrasenaC,
+          decoration: InputDecoration(
+            label: Text('Ingresa tu contraseña'),
+            enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(
+                color: Color.fromRGBO(75, 75, 75, 1),
+                width: 1.0,
+              ),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(
+                color: Color.fromRGBO(163, 16, 75, 1),
+                width: 1.0,
+              ),
+            ),
+            suffixIcon: IconButton(
+              onPressed: () {
+                setState(() {
+                  mostrarPass = !mostrarPass;
+                });
+              },
+              icon: mostrarPass
+                  ? Icon(Icons.remove_red_eye)
+                  : Icon(Icons.visibility_off_rounded),
+            ),
+          ),
+        ),
+        Container(
+          margin: EdgeInsets.all(5),
+          child: Column(
+            children: [
+              FilledButton.icon(
+                onPressed: () => login(context, correoC, constrasenaC),
+                label: Text('Inicia sesión'),
+                icon: Icon(Icons.login),
+              ),
+              TextButton(
+                onPressed: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => RegistroScreen()),
+                ),
+                child: Text('¿No tienes una cuenta? Registrate aquí'),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
-    Navigator.pushNamedAndRemoveUntil(context, "/", (route) => false);
-  } on FirebaseAuthException catch (e) {
-    if (e.code == 'user-not-found') {
-      print('No user found for that email.');
-    } else if (e.code == 'wrong-password') {
-      print('Wrong password provided for that user.');
+  }
+
+  Future<void> login(context, _correo, _contrasena) async {
+    String correo = _correo.text;
+    String contrasena = _contrasena.text;
+    try {
+      final credential = await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: correo,
+        password: contrasena,
+      );
+      Navigator.pushNamedAndRemoveUntil(context, "/", (route) => false);
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        print('No user found for that email.');
+      } else if (e.code == 'wrong-password') {
+        print('Wrong password provided for that user.');
+      }
     }
   }
 }
